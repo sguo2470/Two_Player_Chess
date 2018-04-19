a0f44d6f14326b48eb15b470e77d75fc88c9a901
jfw5160 <jfw5160@E165-WL47.up.ist.local>
2018-04-19 14:46:29 -0400

    Created Grid Class

--- Two_Player_Chess/src/Grid.java
+++ Two_Player_Chess/src/Grid.java
@@ -1,3 +1,16 @@
 public class Grid {
     
+    Space [] spacesArray = new Space[64];
+    
+    //drawGrid;
+    
+    public void drawGrid(){
+        for(int i = 0; i < spacesArray.length; i ++){
+            
+        }
+    }
+    //movePiece;
+    //updateGrid;
+    //
+    
 }
