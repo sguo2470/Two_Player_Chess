b84fdfc9d0c1ce8c9a6109ed3135af547ecc6003
Guo <Guo@104.39.238.252>
2018-04-19 14:38:39 -0400

    Created Chess Pieces

---
+++ Two_Player_Chess/src/Piece.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Guo
+ */
+public class Piece {
+    private String id;
+    private String color;
+    //private int movement;
+
+    public Piece(String id, String color, int movement) {
+        this.id = id;
+        this.color = color;
+        //this.movement = movement;
+    }
+
+    public Piece() {
+    }
+    
+ 
+}
