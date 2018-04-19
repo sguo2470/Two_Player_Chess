865540e7a6d4b8242d6e1a1033b07c12530c5bef
Elisha Wu <Elisha Wu@104.39.14.147>
2018-04-19 14:30:56 -0400

    Added Space class

---
+++ Two_Player_Chess/src/Space.java
@@ -0,0 +1,11 @@
+
+public class Space {
+    private Piece piece;
+    private String locationID;
+
+    public Space(Piece piece, String locationID) {
+        this.piece = piece;
+        this.locationID = locationID;
+    }
+    
+}
