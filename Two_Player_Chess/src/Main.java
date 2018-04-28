c2fdd8cc58d6bac9ae955405e2306860ec97e982
Guo <Guo@104.39.238.252>
2018-04-27 22:45:30 -0400

    Added demo version of drawGrid and created arrays for piece and space

--- Two_Player_Chess/src/Main.java
+++ Two_Player_Chess/src/Main.java
@@ -1,26 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author svg5502
- */
+import java.util.Arrays;
 public class Main {
        
     
     public static void main(String[] args) {
         King whiteKing = new King("wKing", "White", 1);
+        Queen whiteQueen = new Queen("wQueen", "White", 2);
+        Bishop whiteBishop = new Bishop("wBishop", "White", 3);
+        Pawn whitePawn = new Pawn("wPawn", "White", 4);
+        
+        Space a1 = new Space(whiteKing, 1);
+       Space a2 = new Space(whiteQueen, 2);
+       Space a3 = new Space(whiteBishop, 3);
+       Space a4 = new Space(whitePawn, 4);
+     
+      
         System.out.println(whiteKing.toString());
+        Space [] spacesArray = {a1,a2,a3,a4};
+        Piece [] pieceArray = {whiteKing,whiteQueen,whiteBishop,whitePawn};
+       
+
+      
         
-        Grid board = new Grid();
+        Grid board = new Grid(spacesArray, pieceArray);
         board.drawGrid();
-    //Created in iteration 1
+
         Queen blackQueen = new Queen("bQueen", "Black", 1);
-        Space a1 = new Space(null, 2);
-        
+      
         System.out.println(a1.toString());
         a1.setNewPiece(whiteKing);
         System.out.println(a1.toString());
